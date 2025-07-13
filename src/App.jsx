b5e1b509fc05ee97e5193c40cd11670c20c88aa1
silasoyibs <silasoyibs@gmail.com>
2025-07-14 00:23:17 +0100

    "initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route index path="/login" element={<Login />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <Login />
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <div className="text-3xl h-7 bg-amber-400">working</div>;
+}
+
+export default Login;
